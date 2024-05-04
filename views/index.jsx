497c40c31f42eb58f28a6f7b2724b93c4b86208e
TryConor <cjmw5t@umsystem.edu>
2024-05-04 16:58:50 -0500

    index.jsx
    d

---
+++ views/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Define your React components here
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+// Render your React components to the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
